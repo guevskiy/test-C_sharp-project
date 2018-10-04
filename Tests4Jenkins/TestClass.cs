e4d62a5cbd89000833e90d9345db7e8ce7357859
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2018-10-04 14:58:04 +0300

    add test for count groups

--- Tests4Jenkins/TestClass.cs
+++ Tests4Jenkins/TestClass.cs
@@ -13,11 +13,12 @@ namespace Tests4Jenkins
     [TestFixture]
     public class TestClass
     {
-        IWebDriver driver = new ChromeDriver();
+        IWebDriver driver;
 
         [SetUp]
         public void SetUp()
         {
+            driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://addressbook:8087/group.php");
             Thread.Sleep(2000);
         }
@@ -28,8 +29,9 @@ namespace Tests4Jenkins
             driver.Close();
             driver.Quit();
         }
+
         [Test]
-        public void TestMethod()
+        public void TestMethod_001()
         {
             driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
             Thread.Sleep(500);
@@ -39,6 +41,19 @@ namespace Tests4Jenkins
             Thread.Sleep(2000);
             string str = driver.FindElement(By.CssSelector("div#content h1")).Text;
             Assert.AreEqual("Groups", str);
+        }
+
+        [Test]
+        public void TestMethod_002()
+        {
+            driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
+            Thread.Sleep(500);
+            driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys("secret");
+            Thread.Sleep(500);
+            driver.FindElement(By.CssSelector("input[value='Login']")).Click();
+            Thread.Sleep(2000);
+            int nn = driver.FindElements(By.CssSelector("span.group")).Count;
+            Assert.AreEqual(2, nn);
         }
     }
 }
