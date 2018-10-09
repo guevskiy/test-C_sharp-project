8799268d0c8256eac107d18f3f624927d4829368
User-PC\User <artemgue@123software.ru>
2018-10-09 22:29:50 +0300

    test size

--- Tests4Jenkins/TestClass.cs
+++ Tests4Jenkins/TestClass.cs
@@ -19,7 +19,9 @@ namespace Tests4Jenkins
         public void SetUp()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://addressbook:8087/group.php");
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html");
+            //driver.Navigate().GoToUrl("http://addressbook:8087/group.php");
+            driver.Manage().Window.Maximize();
             Thread.Sleep(2000);
         }
 
@@ -31,56 +33,64 @@ namespace Tests4Jenkins
         }
 
         [Test]
-        public void TestMethod_001()
+        public void TestMethod_01()
         {
-            driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys("secret");
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("input[value='Login']")).Click();
-            Thread.Sleep(2000);
-            string str = driver.FindElement(By.CssSelector("div#content h1")).Text;
-            Assert.AreEqual("Groups", str);
+            string sz = driver.Manage().Window.Size.ToString();
+            Console.WriteLine(sz);
+            Assert.AreEqual("{Width=1296, Height=1000}", sz);
         }
 
-        Random rnd = new Random();
+        //[Test]
+        //public void TestMethod_001()
+        //{
+        //    driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys("secret");
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[value='Login']")).Click();
+        //    Thread.Sleep(2000);
+        //    string str = driver.FindElement(By.CssSelector("div#content h1")).Text;
+        //    Assert.AreEqual("Groups", str);
+        //}
 
-        public void createNewGroup()
-        {
-            int n = rnd.Next(1, 99);
-            
-            driver.FindElement(By.CssSelector("input[name='new']")).Click();
+        //Random rnd = new Random();
 
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("input[name='group_name']")).SendKeys("group_name " + n);
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("textarea[name='group_header']")).SendKeys("group_header" + n);
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("textarea[name='group_footer']")).SendKeys("group_footer" + n);
+        //public void createNewGroup()
+        //{
+        //    int n = rnd.Next(1, 99);
 
-            driver.FindElement(By.CssSelector("input[name='submit']")).Click();
-        }
+        //    driver.FindElement(By.CssSelector("input[name='new']")).Click();
 
-        [Test]
-        public void TestMethod_002()
-        {
-            
-            driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys("secret");
-            Thread.Sleep(500);
-            driver.FindElement(By.CssSelector("input[value='Login']")).Click();
-            Thread.Sleep(1000);
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[name='group_name']")).SendKeys("group_name " + n);
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("textarea[name='group_header']")).SendKeys("group_header" + n);
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("textarea[name='group_footer']")).SendKeys("group_footer" + n);
 
-            int nn_befor = driver.FindElements(By.CssSelector("span.group")).Count;
-            createNewGroup();
-            Thread.Sleep(500);
-            driver.FindElement(By.LinkText("group page")).Click();
-            Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[name='submit']")).Click();
+        //}
 
-            int nn_after = driver.FindElements(By.CssSelector("span.group")).Count;
-            Assert.AreEqual(nn_befor+1, nn_after);
-        }
+        //[Test]
+        //public void TestMethod_002()
+        //{
+
+        //    driver.FindElement(By.CssSelector("input[name='user']")).SendKeys("admin");
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[name='pass']")).SendKeys("secret");
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.CssSelector("input[value='Login']")).Click();
+        //    Thread.Sleep(1000);
+
+        //    int nn_befor = driver.FindElements(By.CssSelector("span.group")).Count;
+        //    createNewGroup();
+        //    Thread.Sleep(500);
+        //    driver.FindElement(By.LinkText("group page")).Click();
+        //    Thread.Sleep(500);
+
+        //    int nn_after = driver.FindElements(By.CssSelector("span.group")).Count;
+        //    Assert.AreEqual(nn_befor+1, nn_after);
+        //}
 
     }
 }
