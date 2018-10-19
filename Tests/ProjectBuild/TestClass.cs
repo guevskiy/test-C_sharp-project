3b62624d32989fb6f41444770d4ea9e255347738
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2018-10-19 14:52:23 +0300

    no message

--- Tests/ProjectBuild/TestClass.cs
+++ Tests/ProjectBuild/TestClass.cs
@@ -27,46 +27,47 @@ namespace ProjectBuild
 
       Thread.Sleep(15000);
 
-      //получаем ответ запущенного процесса
-      //StreamReader srIncoming = procCommand.;
-      // выводим результат
-      //Console.WriteLine(srIncoming.ReadToEnd());
-      // закрываем процесс
-      //procCommand.Close();
-      //Console.ReadKey();
+    }
+
+    [Test]
+    public void TestMethod_StartProject_002()
+    {
+
+      driver = new ChromeDriver();
+      driver.Navigate().GoToUrl("http://localhost:4200");
+      driver.Manage().Window.Maximize();
+      driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+
+      //var elem = driver.FindElement(By.CssSelector("div.card div.card-header"));
+
+      //for (int i = 0; i < 20; i++)
+      //{
+      //  if (driver.FindElements(By.CssSelector("div.card div.card-header")).Count < 0)
+      //  {
+      //    Thread.Sleep(1500);
+      //    driver.Navigate().Refresh();
+          
+      //  }
+      //  else
+      //  {
+      //    Assert.AreEqual("Welcome", elem.Text);
+      //  }
+      //}
+
+
+      int i = 0;
+      while (i < 1)
+      {
+        Thread.Sleep(2000);
+        driver.Navigate().Refresh();
+        i = driver.FindElements(By.CssSelector("div.card div.card-header")).Count;
+      }
+
+      Assert.AreEqual("Welcome", driver.FindElement(By.CssSelector("div.card div.card-header")).Text);
 
     }
 
-    //[Test]
-    //public void TestMethod_StartProject_002()
-    //{
-      
-    //  driver = new ChromeDriver();
-    //  driver.Navigate().GoToUrl("http://localhost:4200");
-    //  driver.Manage().Window.Maximize();
-    //  driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-
-    //  var elem = driver.FindElement(By.CssSelector("div.card div.card-header"));
-
-      
-    //  Assert.AreEqual("Welcome", elem.Text);
-    //}
-
-
-    //public string GetElemName(int nn)
-    //{
-    //  WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-
-    //  string labelName = wait.Until(
-    //    (IWebDriver d) =>
-    //    {
-    //      string name = driver.FindElement(By.CssSelector("div.card div.card-header")).Text;
-    //      return name != "" ? name : null;
-    //    }
-    //    );
-    //  return labelName;
-    //}
 
   }
 }
